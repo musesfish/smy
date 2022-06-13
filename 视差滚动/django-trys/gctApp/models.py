fe9929ef835d02b28f290982684187bbacb50efa
amy <sumeiyuan@chinasofti.com>
2022-06-13 10:37:44 +0800

    smy

--- 视差滚动/django-trys/gctApp/models.py
+++ 视差滚动/django-trys/gctApp/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Test(models.Model):
     name = models.CharField(max_length=20)
 
 class Imgs(models.Model):
-    name = models.CharField(max_length=20)
-    path = models.CharField(max_length=100)
+    name = models.CharField(max_length=200,verbose_name=u"标题")
+    author = models.CharField(max_length=100,verbose_name=u"作者",default="amy")
+    content = models.CharField(max_length=1000,verbose_name=u"内容",default="")
+    path = models.ImageField(upload_to='img_url/',verbose_name=u"图片地址")
+    time = models.DateTimeField(default=datetime.now(),verbose_name=u"添加时间")
